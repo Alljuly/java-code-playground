f3944f80cc579758734435adeaaa48bc5e3e018c
Alice Julia <86618692+Alljuly@users.noreply.github.com>
2025-01-23 14:52:40 +0000

    classe teste de animais

--- poo/interface/animais/entidades/Animal.java
+++ poo/interface/animais/entidades/Animal.java
@@ -1,18 +1,22 @@
 abstract class Animal {
     String nome;
     int idade;
-    float peso;
-    float tamanho;
+    double peso;
+    double tamanho;
 
     public Animal(
         String nome,
         int idade,
-        float peso, 
-        float tamanho)
+        double peso, 
+        double tamanho)
     {
         this.nome = nome;
         this.idade = idade;
         this.peso = peso;
         this.tamanho = tamanho;
+    }
+
+    public String getNome(){
+        return nome;
     }
 }
